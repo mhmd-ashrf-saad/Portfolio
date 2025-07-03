7b38a704ab31d021d56585e0b739157aa27f4be3
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-07-03 21:10:37 +0300

    header social links

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
+interface Link{
+  name: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -13,4 +18,30 @@ export class Header {
     const searchForm = document.querySelector('.search-form');
     searchForm?.classList.toggle('active');
   }
+  links :Link[]=[
+    {
+      name:"instagram",
+      link:"https://www.instagram.com/_mhmdashrf/"
+    },
+    {
+      name:"linkedin",
+      link:"https://www.linkedin.com/in/mohamed-ashraf-abdelbaset/"
+    },
+    {
+      name:"github",
+      link:"https://github.com/mhmd-ashrf-saad"
+    },
+  ];
+  get instagramLink() {
+    return this.links.find(link => link.name === 'instagram');
+  }
+
+  get linkedinLink() {
+    return this.links.find(link => link.name === 'linkedin');
+  }
+
+  get githubLink() {
+    return this.links.find(link => link.name === 'github');
+  }
 }
+
