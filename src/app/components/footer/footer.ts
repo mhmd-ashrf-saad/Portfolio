8527674780724b5e02900ef09d5451c95e2f36d2
Mohamed Ashraf <esmymohamed2@gmail.com>
2025-07-03 18:06:37 +0300

    footer links

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 
-
+interface Link{
+  name: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +12,31 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css'
 })
 export class Footer {
-links :string[]=[
-  "www.google.com","inaASLKa","ASDL","ASKDL","SAK"
+  currentYear = new Date().getFullYear();
+
+  links :Link[]=[
+  {
+    name:"instagram",
+    link:"https://www.instagram.com/_mhmdashrf/"
+  },
+  {
+    name:"linkedin",
+    link:"https://www.linkedin.com/in/mohamed-ashraf-abdelbaset/"
+  },
+  {
+    name:"github",
+    link:"https://github.com/mhmd-ashrf-saad"
+  },
 ];
+  get instagramLink() {
+    return this.links.find(link => link.name === 'instagram');
+  }
+
+  get linkedinLink() {
+    return this.links.find(link => link.name === 'linkedin');
+  }
+
+  get githubLink() {
+    return this.links.find(link => link.name === 'github');
+  }
 }
